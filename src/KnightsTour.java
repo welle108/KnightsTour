0a43c1f8061fb6927c2095a29135503c2f420fb5
Scott Weller <welle108@mail.chapman.edu>
2019-04-22 23:44:42 -0700

    -Adds Brute force method

--- src/KnightsTour.java
+++ src/KnightsTour.java
@@ -1,5 +1,136 @@
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class KnightsTour {
+    private static Position init;
+    private static boolean running = true;
+    private static int full;
+    private static int dimen;
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Options options = new Options();
+        options.addOption("h",false, "Enables heuristic knights tour with Warnsdorf algorithm");
+        CommandLineParser parser = new DefaultParser();
+        try{
+            CommandLine cmd = parser.parse(options, args);
+            if(cmd.hasOption("h")){
+            dimen = Integer.parseInt(args[1]);
+            full = dimen*dimen;
+            //Run heuristic analysis with Warnsdorf
+
+            }else{
+            dimen = Integer.parseInt(args[0]);
+            init = new Position(0,0);
+            //Brute force approach
+            int[][]tour = bruteTour();
+            printTour(tour);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
+
+
+    public static int[][] bruteTour(){
+        ArrayList<Position> current_tour = new ArrayList<>();
+        current_tour.add(init);
+        int[][] visited = new int[dimen][dimen];
+        for(int[] row : visited){
+            Arrays.fill(row, 0);
+        }
+        visited[init.getX()][init.getY()] = 1;
+        bruteBacktrack(current_tour, visited);
+         int[][] tour = new int[dimen][dimen];
+         int count = full;
+         for(int i = 0; i < count; i++){
+             Position cur = current_tour.get(i);
+             tour[cur.getX()][cur.getY()] = i + 1;
+         }
+
+         return tour;
+
+
+    }
+
+    private static void bruteBacktrack(ArrayList<Position> tour, int[][]visited){
+
+        if(tour.size() == full){
+            running = false;
+        }
+        else{
+            ArrayList<Position> moves = checkMoves(tour, visited);
+            for(Position p : moves){
+                tour.add(p);
+                visited[p.getX()][p.getY()] = 1;
+                bruteBacktrack(tour, visited);
+                if(!running){
+                    return;
+                }
+                else{
+                    tour.remove(p);
+                    visited[p.getX()][p.getY()] = 0;
+                }
+            }
+        }
+    }
+
+    public void heuristicTour(){}
+
+    private static ArrayList<Position> checkMoves(ArrayList<Position> tour, int[][] visited){
+        ArrayList<Position> possible_moves = new ArrayList<>();
+        Position current_position = tour.get(tour.size()-1);
+        int x = current_position.getX();
+        int y = current_position.getY();
+
+        if(x-2 >= 0 && y+1 < dimen && visited[x-2][y+1] != 1){
+            possible_moves.add(new Position(x-2,y+1));
+        }else if(x-2 >= 0 && y-1 >= 0 && visited[x-2][y-1] != 1){
+            possible_moves.add(new Position(x-2,y-1));
+        }
+        else if(x-1 >= 0 && y+2 < dimen && visited[x-1][y+2] != 1){
+            possible_moves.add(new Position(x-1,y+2));
+        }
+        else if(x-1 >= 0 && y-2 >= 0 && visited[x-1][y-2] != 1){
+            possible_moves.add(new Position(x-1,y-2));
+        }
+        else if(x+1 < dimen && y-2 >= 0 && visited[x+1][y-2] != 1){
+            possible_moves.add(new Position(x+1,y-2));
+        }
+        else if(x+1 < dimen && y+2 < dimen && visited[x+1][y+2] != 1){
+            possible_moves.add(new Position(x+1,y+2));
+        }
+        else if(x+2 < dimen && y-1 >= 0 && visited[x+2][y-1] != 1){
+            possible_moves.add(new Position(x+2,y-1));
+        }
+        else if(x+2 < dimen && y+1 < dimen && visited[x+2][y+1] != 1){
+            possible_moves.add(new Position(x+2,y+1));
+        }
+        return possible_moves;
+    }
+
+    public static void printTour(int[][] tour){
+        for(int i = 0; i<tour.length; i++){
+            for(int j = 0; j<tour.length; j++){
+                if(j == tour.length-1){
+                    System.out.print(" | " + tour[i][j]+" | " + "\n");
+                }else{
+                    System.out.print(" | " + tour[i][j]+" | ");
+                }
+
+            }
+        }
+    }
+
 }
+
---
+++ src/Position.java
@@ -0,0 +1,18 @@
+public class Position {
+    private int x_cor;
+    private int y_cor;
+
+    public Position(int x_cor, int y_cor) {
+
+        this.x_cor = x_cor;
+        this.y_cor = y_cor;
+    }
+
+    public int getX() {
+        return x_cor;
+    }
+
+    public int getY() {
+        return y_cor;
+    }
+}
